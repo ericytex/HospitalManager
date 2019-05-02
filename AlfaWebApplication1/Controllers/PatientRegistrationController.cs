934c173546287396c8ad4effaa70a9cddf8ae6a5
eric watyekele <ewatyeks@gmail.com>
2019-05-02 19:20:17 +0300

    latest changes in connect to database. updated conection string

--- AlfaWebApplication1/Controllers/PatientRegistrationController.cs
+++ AlfaWebApplication1/Controllers/PatientRegistrationController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Configuration;
 
 namespace AlfaWebApplication1.Controllers
 {
     public class PatientRegistrationController : Controller
     {
+
+        //sqlconnection
+
+        private SqlConnection con;
+
+        //To Handle connection related activities
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+
         // GET: PatientRegistration
         public ActionResult Index()
         {
---
+++ AlfaWebApplication1/Models/PatientRegisterModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlfaWebApplication1.Models
+{
+    public class PatientRegisterModel
+    {
+
+        [System.ComponentModel.DataAnnotations.Display(Name = "Id")]
+        public int id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Middle name is required.")]
+        public string MiddleName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "First name is required.")]
+        public string FirstName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Patient File Number is required.")]
+        public string PatientFileNumber { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Enrolled Date is required.")]
+        public string EnrolledDate { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Marital Status is required.")]
+        public string MaritalStatus { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Date of Birth is required.")]
+        public string DateOfBirth { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Age is required.")]
+        public string Age { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Gender is required.")]
+        public string Gender { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "National ID Number is required.")]
+        public string NationalIDNo { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Cell Phone Number is required.")]
+        public string CellPhoneNumber { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Home Phone Number is required.")]
+        public string HomePhoneNumber { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Occupation is required.")]
+        public string Occupation { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Employer is required.")]
+        public string Employer { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Employer Phone Number is required.")]
+        public string EmployerPhoneNumber { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Reason for visit is required.")]
+        public string ReasonForVisit { get; set; }
+    }
+}
